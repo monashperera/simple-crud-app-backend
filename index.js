c41517879cad662d504bc0ea819e5f91f80625ba
Monash Perera <32927852+monashperera@users.noreply.github.com>
2025-11-15 14:10:02 +0530

    feat(api): add endpoint to get product by ID

--- index.js
+++ index.js
@@ -17,9 +17,12 @@ app.get('/api/products', async (req, res) => {
     }
 });
 
-app.get('/apt/products/:id', async (req, res) => {
+app.get('/api/products/:id', async (req, res) => {
     try {
-        
+        const {id} = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+
     } catch (error) {
         res.status(500).json({message: error.message});
     }
