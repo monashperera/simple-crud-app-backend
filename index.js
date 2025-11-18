199deefbcd069aa4e1108b14cd4650958ab71d68
Monash Perera <32927852+monashperera@users.noreply.github.com>
2025-11-18 14:11:43 +0530

    feat(api): change port 3000 to 5000

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ app.get('/', (req, res) => {
 mongoose.connect(process.env.MONGO_URL)
     .then(() => {
         console.log("connected to database");
-        const PORT = process.env.PORT || 3000;
+        const PORT = process.env.PORT || 5000;
         app.listen(PORT, () => {
-            console.log("Server running on port 3000");
+            console.log("Server running on port 5000");
         });
     })
     .catch((err) => {
